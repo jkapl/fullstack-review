7f3e72ad61ab88333e6f3040c548962f929d6eb6
Joel <joelkaplan1@gmail.com>
2019-06-27 16:15:16 -0700

    display repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
+  <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+  </div>
+  <ul>
+    {props.repos.map ( repo => (
+      <li>{repo}</li>
+    ))}
+  </ul>
   </div>
 )
 
